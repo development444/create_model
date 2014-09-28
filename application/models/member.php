2f068ddef9c05fcd39c04b0c0a9a4d7011e8fd61
momentumsubash <suacharya@olivemedia.co>
2014-09-28 13:56:23 +0545

    few changes

--- application/models/member.php
+++ application/models/member.php
@@ -1,5 +1,59 @@
 <?php
-class Membership_model extends CI_Model {
+class Member extends CI_Model {
+
+
+    var $check=false; //Create a variable for checking is already member or not
+    var $varified=false;// check if created member is varified or not
+   public /**
+   * 
+   */
+   class Create_member extends Member
+   {
+      
+      function __construct(argument)
+      {
+         # code...
+        $this->check_status();
+
+          if ($check=false) {//check  the condition 
+         # code...
+
+         // create member using database
+            $this->create_member;
+      }
+      }
+
+
+      public function create_member(){
+         //create member in db
+
+         if (condition) {//check validation is sucessful or not
+            # code...
+            //1. username should be of more than 4 character and les than 12
+            //2. no field should be blank or empty
+            //
+
+         }
+
+
+      }
+
+
+      public function check_status($check){
+
+         //retrive info from database to check if username exist or not
+
+
+        if (condition) { //if found in database set check var  to true 
+
+           # code...
+        }
+        return $check;
+    
+
+
+      }
+   }
 
 
 
