40554c84bbc880fbd46365c3bed42968c7d43b4f
sandyp143 <linktome_143@gmail.com>
2014-09-30 16:01:48 +0545

    showrrom functions

--- application/models/showroom.php
+++ application/models/showroom.php
@@ -1,43 +1,120 @@
 
 <?php
- class showroom extends CI_model
+ class showroom 
  {
- 		public function __construct($attributes=array())
- 		{
+ 		private $showroom_id;
+ 		private$showroom_name;
+ 		private $model_name;
+ 		private $location;
+ 		private $is_deleted;
+ 		private $is_active;
 
- 			parent::__construct ($attributes);
- 		}
 
  		public function set_showroomname($name)
  		 {
 
-		    	$name = trim($name);
+		    	$this->showroom_name = trim($name);
 
 				if($name == '') {
-           		throw new Exception('A name is required to create a showroom.');
+           		     throw new Exception('A name is required to create a showroom.');
         		}
 
-				$this->assign_attribute('name', $name);
+				
 	    }
 
 	    public function set_showroomlocation($name)
  		 {
 
-		    	$name = trim($name);
+		    	$this->location = trim($name);
 
 				if($name == '') {
-           		throw new Exception('A location is required to create a showroom.');
+           		     throw new Exception('A location is required to create a showroom.');
         		}
 
-				$this->assign_attribute('location', $name);
+				
 	    }
+
+	    public function set_modelname($name)
+ 		 {
+
+		    	$this->model_name = trim($name);
+
+				if($name == '') {
+           		     throw new Exception('A model name is required to create a showroom.');
+        		}
+
+				
+	    }
+
+	    public function is_deleted() {
+		           return ($this->is_deleted) ? true : false;
+	             }
+
+
+
+
+	    public function delete() {
+
+		        if($this->is_deleted()) {
+			         throw new exception('deleted');
+		        }
+
+		        $this->is_deleted = 1;
+		        return $this;
+	            }
+
+
+
+	    public function is_active() {
+		         return ($this->is_active) ? true : false;
+	             }      
+
+
+
+	    public function activate() {
+
+		        if($this->is_deleted()) {
+			         $this->throw_base_exception('deleted');
+		         }
+
+		        if($this->is_active()) {
+			        throw new exception('active');
+		         }
+
+                $this->is_active = 1;
+                return $this;
+		        
+	           }
+
+
+	     public function check_model($model)
+	     {
+
+	     		if(exists(array('model_name'=>$model)))
+	     		{
+	     			 throw new exception('This $model model exists in the showroom');
+	     		}
+	     		else
+	     		{
+
+	     			 throw new exception('This $model model  does not exists in the showroom');
+	     		}
+
+	     }
+
+
+
+
 
 	    public static function create($params) {
 
 		$showroom = new showroom;
 
-		$showrroom->name = array_key_exists('name', $params) ? $params['name'] : null;
-		$showroom->location = array_key_exists('location', $params) ? $params['location'] : null;
+		$showroom->name = $this->set_showroomname($params[name]);
+		$showroom->location = $this->set_showroomlocation($params[location]);
+		$showroom->model_name = $this->set_modelname($params[model_name]);
+		$course->is_active = 1;
+		$course->is_deleted = 0;
 		
 
 		return $showroom;
@@ -45,12 +122,7 @@
 
 
 
-	    public assign_attribute('attribute_key',$value)
-	    {
-
-	    	$this->attributes['attribute_key'] = $value;
- 
-	    }
+	    
 
 
 
