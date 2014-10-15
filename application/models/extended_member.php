6d185b5317449087f35a47c5abb4fe3955c3d079
subash acharya <suacharya@olivemedia.co>
2014-10-15 15:44:33 +0545

    some change

--- application/models/extended_member.php
+++ application/models/extended_member.php
@@ -5,34 +5,59 @@
       function __construct(argument)
       {
          # code...
-        $this->check_status();
-
-          if ($check=false) {//check  the condition 
-         # code...
-
-         // create member using database
-            $this->create_member();
+       
       }
-      }
+function set_first_name($params){
+if($params == '') {
+                   throw new Exception('A first  name is required to create a showroom.');
+            }
 
+
+}
+function set_last_name($params){
+if($params == '') {
+                   throw new Exception('A first  name is required to create a showroom.');
+            }
+
+
+}
+function set_email($params){
+if($params == '') {
+                   throw new Exception('A first  name is required to create a showroom.');
+            }
+
+
+}
 
       public function create_member($array){
 
          //create member in db
        
+ $this->check_status();
 
+         
+           
+    
 
         $this->validate($array)
 
          if (condition is true) {//check validation is sucessful or not if true create member else give eroor message.
+
             # code...
           
             //
+           if ($check=false) {//check  the condition 
+         # code...
+
+         // create member using database
            $member= new member();// add all fields  like f_name,l_name and email into new member
+           $this->$first_name->array_key_exists('first_name', $params) ? $params['email'] : "";
+            $this->$last_name->array_key_exists('last_name', $params) ? $params['last_name'] : "";
+             $this->$email->array_key_exists('email', $params) ? $params['email'] : "";
            $this->send_confirm_mail();
 
          }
-
+        }
 
 
       }
